d52e2c594c91d214b44e0aaef31caceb1b05c0ae
vickeysangwan1 <vickeysangwan007@gmail.com>
2021-04-25 22:56:36 +0530

    review autoplay added

--- krp-v1/src/components/Review.js
+++ krp-v1/src/components/Review.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { appContext } from "../context/Context";
 import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 import "./styles/review.scss";
@@ -7,39 +7,62 @@ const Review = () => {
   const { review } = React.useContext(appContext);
   const [index, setIndex] = useState(0);
 
-  const nextReview = () => {
-    if (index >= review.length - 1) {
-      setIndex(0);
-    } else {
-      setIndex(index + 1);
-    }
-  };
-  const prevReview = () => {
-    if (index <= 0) {
+  useEffect(() => {
+    if (index < 0) {
       setIndex(review.length - 1);
-    } else {
-      setIndex(index - 1);
     }
-  };
+    if (index > review.length - 1) {
+      setIndex(0);
+    }
+  }, [index, review]);
 
-  const { id, name, job, image, text } = review[index];
+  useEffect(() => {
+    let slider = setInterval(() => setIndex(index + 1), 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
   return (
-    <section className="review" key={id}>
-      <div className="review-container">
-        <div>
-          <h2>{text}</h2>
-          <h3>{name}</h3>
-          <p>{job}</p>
-        </div>
-        <button onClick={prevReview}>
-          <IoIosArrowBack />
-        </button>
-        <button onClick={nextReview}>
-          <IoIosArrowForward />
-        </button>
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
       </div>
-      <div className="img-container">
-        <img src={image} alt={name} />
+      <div className="section-center">
+        {review.map((item, reviewIndex) => {
+          const { id, name, job, image, text } = item;
+          let position = "nextSlide";
+
+          if (reviewIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            reviewIndex === index - 1 ||
+            (index === 0 && reviewIndex === review.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+
+          return (
+            <article className={position} key={id}>
+              <div className="info">
+                <h2>{text}</h2>
+                <h3>{name}</h3>
+                <p>{job}</p>
+                <button onClick={() => setIndex(index - 1)}>
+                  <IoIosArrowBack />
+                </button>
+                <button onClick={() => setIndex(index + 1)}>
+                  <IoIosArrowForward />
+                </button>
+              </div>
+              <div className="img-container">
+                <img src={image} alt={name} />
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
