5bd54a96779a373e7c97c0bbe538a4ac3417ab64
vickeysangwan1 <vickeysangwan007@gmail.com>
2021-05-05 12:58:46 +0530

    navbar active class added

--- krp-v1/src/components/Navbar.js
+++ krp-v1/src/components/Navbar.js
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from "react";
 // import logo from "../images/logo.jpeg";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./styles/navbar.scss";
 
 const Navbar = () => {
   const [scroll, setScroll] = useState(false);
 
+  //*active nav-links
+  const location = useLocation();
+
+  const { pathname } = location;
+
+  const splitLocation = pathname.split("/");
+  console.log(splitLocation);
+  //*active nav-links
+
+  //* scroll logic
   const handleScroll = () => {
     const offSet = window.scrollY;
     if (offSet >= 80) {
@@ -21,6 +31,7 @@ const Navbar = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, [scroll]);
+  //* scroll logic
 
   return (
     <section className={scroll ? "navbar scroll" : "navbar"}>
@@ -32,16 +43,34 @@ const Navbar = () => {
         </div>
         <div className="pages">
           <ul>
-            <Link className="nav-list" to="/">
+            <Link
+              className={`nav-list ${splitLocation[1] === "" ? "active" : ""}`}
+              to="/"
+            >
               Home
             </Link>
-            <Link className="nav-list" to="/about">
+            <Link
+              className={`nav-list ${
+                splitLocation[1] === "about" ? "active" : ""
+              }`}
+              to="/about"
+            >
               about
             </Link>
-            <Link className="nav-list" to="/shop">
+            <Link
+              className={`nav-list ${
+                splitLocation[1] === "shop" ? "active" : ""
+              }`}
+              to="/shop"
+            >
               shopping
             </Link>
-            <Link className="nav-list" to="/contact">
+            <Link
+              className={`nav-list ${
+                splitLocation[1] === "contact" ? "active" : ""
+              }`}
+              to="/contact"
+            >
               Contact us
             </Link>
           </ul>
