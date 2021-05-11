bda0b2897f3d24fc0c1f1a405c34ef1f1046ad17
vickeysangwan1 <vickeysangwan007@gmail.com>
2021-05-11 17:00:28 +0530

    navbar

---
+++ krp-v1/src/components/WhyKRP.js
@@ -0,0 +1,32 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import "./styles/whyKRP.scss";
+
+const About = () => {
+  return (
+    <section className="about-section">
+      <article className="about-info">
+        <div className="heading">
+          <h2>why krp?</h2>
+          <div className="line"></div>
+        </div>
+        <div className="text">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </article>
+    </section>
+  );
+};
+
+export default About;
