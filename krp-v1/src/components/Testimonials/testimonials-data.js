1d8a8530114df1755a7f9e3d93b9d2f495e7d220
Ankit Kumar <ak248100@gmail.com>
2021-05-22 19:55:08 +0530

    contact us added

--- krp-v1/src/components/Testimonials/testimonials-data.js
+++ krp-v1/src/components/Testimonials/testimonials-data.js
@@ -5,7 +5,7 @@ const reviews = [
     title: "manager at ktm",
     image:
       "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVvcGxlfGVufDB8MXwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras efficitur pharetra ante sit amet commodo, consectetur adipiscing elit. ",
+    text: "Lorem ipsum lorem dolor sit amet, consectetur adipiscing elit. Cras efficitur pharetra ante sit amet commodo, consectetur adipiscing elit. ",
   },
   {
     id: 2,
