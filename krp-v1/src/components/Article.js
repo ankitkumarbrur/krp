d2b184374c50a47709fb4209f1a68c4807d2b5a7
Bhagesh <dbhagesh@gmail.com>
2021-05-22 20:26:58 +0530

    NONE

--- krp-v1/src/components/Article.js
+++ krp-v1/src/components/Article.js
@@ -14,7 +14,7 @@ export default function Article() {
                     <div className="article-left">
                         <h2 className="title">Kaulson Racing</h2>
                         <p className="news">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore recusandae saepe tempora natus numquam reiciendis porro itaque aperiam deleniti illum assumenda necessitatibus dolorum quia perferendis iusto ratione, ducimus atque vel libero perspiciatis similique. Aliquid omnis iure velit reiciendis enim obcaecati fuga totam. Fugiat impedit esse repellat nemo aspernatur error sunt tenetur voluptatibus temporibus culpa? Pariatur, architecto, vel consequatur officia nihil dignissimos autem quas dolore, magnam iste distinctio non quos cupiditate.</p>
-                        <h4 className="bottom">BBC NEWS | 23 May 2021 | Kaulson |</h4>
+                        <h4 className="bottom">BBC NEWS | 23 May 2021 | Kaulson </h4>
                     </div>
                 </div>
                 <div className="article-right">
@@ -27,11 +27,11 @@ export default function Article() {
                         <h3 className="article-title">New Launch</h3>
                     </div>
                     <div className="article-div3">
-                        <div className="article-number">02</div>
+                        <div className="article-number">03</div>
                         <h3 className="article-title">Kaulson Racing</h3>
                     </div>
                     <div className="article-div4">
-                        <div className="article-number">02</div>
+                        <div className="article-number">04</div>
                         <h3 className="article-title">Suzuki Motorcorp</h3>
                     </div>
 
--- krp-v1/src/components/Shopping/shopping-data.js
+++ krp-v1/src/components/Shopping/shopping-data.js
@@ -9,37 +9,37 @@ const shopping = [
     id: 2,
     title: "gloves",
     image:
-      "https://images.unsplash.com/photo-1571819507488-0e1dfe7cc22d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGVsbWV0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      "https://source.unsplash.com/featured/?gloves",
   },
   {
     id: 3,
-    title: "jackets",
+    title: "model",
     image:
-      "https://images.unsplash.com/photo-1571819507488-0e1dfe7cc22d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGVsbWV0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      "https://source.unsplash.com/featured/?model",
   },
   {
     id: 4,
     title: "tires",
     image:
-      "https://images.unsplash.com/photo-1571819507488-0e1dfe7cc22d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGVsbWV0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      "https://source.unsplash.com/featured/?tyres",
   },
   {
     id: 5,
     title: "gears",
     image:
-      "https://images.unsplash.com/photo-1571819507488-0e1dfe7cc22d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGVsbWV0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      "https://source.unsplash.com/featured/?gears",
   },
   {
     id: 6,
     title: "shoes",
     image:
-      "https://images.unsplash.com/photo-1571819507488-0e1dfe7cc22d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGVsbWV0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      "https://source.unsplash.com/featured/?shoes",
   },
   {
     id: 7,
-    title: "gears",
+    title: "jacket",
     image:
-      "https://images.unsplash.com/photo-1571819507488-0e1dfe7cc22d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGVsbWV0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      "https://source.unsplash.com/featured/?jacket",
   },
 ];
 
